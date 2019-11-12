6b48baf9c0ec17eaff3a93c045190d9aa8aab189
Rok <rok.zitko@ijs.si>
2019-11-13 00:41:34 +0100

    comment added

--- c++/nrg-lib.h
+++ c++/nrg-lib.h
@@ -4,7 +4,7 @@
 #include <string>
 
 void run_nrg_master();
-void run_nrg_slave();
+void run_nrg_slave(); // note: only defined if compled using NRG_MPI
 void set_workdir(std::string workdir);
 
 #endif
