8eeda4d2685419d6164cc2288770224751b4f2f2
Rok Zitko <rok.zitko@ijs.si>
2020-03-09 17:14:07 +0100

    std:: prepended

--- c++/nrg-lib.h
+++ c++/nrg-lib.h
@@ -10,7 +10,7 @@
 void run_nrg_master();
 void run_nrg_slave(); // note: only defined if compled using NRG_MPI
 void set_workdir(const std::string &workdir);
-void print_about_message(ostream &s = std::cout);
+void print_about_message(std::ostream &s = std::cout);
 
 namespace time_mem {
   void timing_report();
