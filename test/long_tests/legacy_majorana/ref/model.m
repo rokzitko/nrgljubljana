a5b39aa65953029f8be7a569a1fe6d2dcaa3de76
Rok Zitko <rok.zitko@ijs.si>
2020-10-06 15:37:05 +0200

    long tests

---
+++ test/long_tests/legacy_majorana/ref/model.m
@@ -0,0 +1,8 @@
+def1ch[1];
+
+op1 = nc[ (f[CR, 0, DO]+f[AN,0, DO])/Sqrt[2], (d[CR, DO] + d[AN, DO])/Sqrt[2] ];
+
+(* Hc = gammaPolCh[1] hop[f[0], d[]]; *)
+Hc = gammaPolCh[1] (hop[f[0], d[], UP] + op1 + conj[op1]);
+
+H = H0 + H1 + Hc;
