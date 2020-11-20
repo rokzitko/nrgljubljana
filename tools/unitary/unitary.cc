beba79cfe403c46d49e18cbf94ef6f9914454be4
Rok Zitko <rok.zitko@ijs.si>
2020-11-20 12:56:11 +0100

    refactorization of unitary

--- tools/unitary/unitary.cc
+++ tools/unitary/unitary.cc
@@ -2,7 +2,6 @@
 
 int main(int argc, char *argv[]) {
   using namespace NRG::Unitary;
-  parse_param(argc, argv);
-  about();
-  run(argc, argv);
+  Unitary unit(argc, argv);
+  unit.run(argc, argv);
 }
--- tools/unitary/unitary.hpp
+++ tools/unitary/unitary.hpp
@@ -37,109 +37,84 @@ namespace NRG::Unitary {
 using namespace std;
 using namespace boost::numeric;
 
-bool quiet            = false;
-bool verbose          = false;
-bool veryverbose      = false;
-bool transpose_first  = false;
-bool transpose_last   = false;
-double scale_factor   = 1.0;
-char *output_filename = nullptr;
-
-bool input_ac_bin = false;
-
-const int OUTPUT_PREC = 18;
-double CHOP_TOL       = 1e-14;
-
-using MAT = ublas::matrix<double>;
-
-auto count_breaks(const std::string &s) {
-  auto found = 0;
-  for (auto i = 0; i < s.length(); i++)
-    if (isspace(s[i]))
-      // Only non-consecutive spaces are counted
-      if (i == 0 || (i > 0 && !isspace(s[i-1]))) found++;
-  return found;
+inline auto count_words_in_string(const std::string &s) {
+  std::stringstream stream(s);
+  return std::distance(std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>());
 }
 
-auto get_dims(std::ifstream &F) {
+// Determine the matrix dimensions from a stream of rows of white-space-separated tabulated values
+inline auto get_dims(std::ifstream &F) {
   auto dim1 = 0; // number of rows
   auto dim2 = 0; // number of columns
   while (F.good()) {
     std::string s;
     std::getline(F, s);
     if (!F.fail()) {
-      auto n = count_breaks(s);
-      if (dim2>0 && dim2 != n+1) throw std::runtime_error("All matrix rows must be equally long");
-      dim2 = n+1;
+      auto n = count_words_in_string(s);
+      if (dim2 > 0 && dim2 != n) throw std::runtime_error("All matrix rows must be equally long");
+      dim2 = n;
       dim1++;
     }
   }
   return std::make_pair(dim1, dim2);
 }
 
-auto read_matrix_text_data(std::ifstream &F, const size_t dim1, const size_t dim2) {
+using MAT = ublas::matrix<double>;
+
+// Read dim1 x dim2 matrix from stream. Use function next_value to extract consecutive values.
+template<typename FNC>
+auto read_matrix_data(std::ifstream &F, FNC next_value, const size_t dim1, const size_t dim2, const bool check_is_finite = true) {
   MAT M(dim1, dim2);
   for (auto i = 0; i < dim1; i++) {
     for (auto j = 0; j < dim2; j++) {
-      double x;
-      F >> x;
-      assert(finite(x));
+      const auto x = next_value(F);
+      if (check_is_finite && !finite(x)) throw std::runtime_error("Non-finite number detected.");      
       M(i, j) = x;
     }
   }
-  if (veryverbose) std::cout << M << std::endl;
   if (F.fail()) throw std::runtime_error("read_matrix_text() failed. Input corrupted?");
   return M;
 }
 
-auto read_matrix_bin_data(std::ifstream &F, const size_t dim1, const size_t dim2) {
-  MAT M(dim1, dim2);
-  for (auto i = 0; i < dim1; i++) {
-    for (auto j = 0; j < dim2; j++) {
-      double x;
-      F.read((char *)&x, sizeof(double));
-      if (!F) throw std::runtime_error("Failed reading binary data.");
-      assert(finite(x));
-      M(i, j) = x;
-    }
-  }
-  if (veryverbose) std::cout << M << std::endl;
-  if (F.fail()) throw std::runtime_error("read_matrix_bin() failed. Input corrupted?");
-  return M;
-}
-
-auto read_text_matrix(const std::string &filename) {
-  auto F = safe_open_for_reading(filename);
+// Read a matrix from stream (text)
+inline auto read_matrix_text(const std::string &filename, const bool verbose) {
+  auto F = safe_open_for_reading(filename, false);
   const auto [dim1, dim2] = get_dims(F);
   if (verbose) std::cout << filename << " [" << dim1 << " x " << dim2 << "]" << std::endl;
   F.clear();
   F.seekg (0, ios::beg);
-  return read_matrix_text_data(F, dim1, dim2);
+  return read_matrix_data(F, [](auto &F) { double x; F >> x; return x; }, dim1, dim2);
 }
 
-auto read_bin_matrix(const std::string &filename) {
+// Read a matrix from stream (binary). Format: two unit32_t for matrix size, followed by
+// dim1 x dim2 double values;
+inline auto read_matrix_bin(const std::string &filename, const bool verbose) {
   auto F = safe_open_for_reading(filename, true);
   uint32_t dim1, dim2;
   F.read((char *)&dim1, sizeof(uint32_t));
   F.read((char *)&dim2, sizeof(uint32_t));
   if (verbose) std::cout << filename << " [" << dim1 << " x " << dim2 << "]" << std::endl;
-  return read_matrix_bin_data(F, dim1, dim2);
+  return read_matrix_data(F, [](auto &F) { double x; F.read((char *)&x, sizeof(double)); return x; }, dim1, dim2);
 }
 
-auto read_matrix(const std::string &filename, const bool bin = false) {
-  return bin ? read_bin_matrix(filename) : read_text_matrix(filename);
+inline auto read_matrix(const std::string &filename, const bool bin = false, const bool verbose = false, const bool veryverbose = false) {
+  auto M = bin ? read_matrix_bin(filename, verbose) : read_matrix_text(filename, verbose);
+  if (veryverbose) std::cout << M << std::endl;
+  return M;
 }
 
-void save_matrix(const std::string &filename, const MAT &M) {
+inline void save_matrix(const std::string &filename, const MAT &M, const bool verbose = false,
+                        const double chop_tol = 1e-14, const int output_prec = 18)
+{
   if (verbose) std::cout << "Saving result to " << filename << std::endl;
   auto F = safe_open(filename);
-  F << std::setprecision(OUTPUT_PREC);
+  F << std::setprecision(output_prec);
   const auto dim1 = M.size1();
   const auto dim2 = M.size2();
   for (auto i = 0; i < dim1; i++) {
     for (auto j = 0; j < dim2; j++) {
       double val = M(i, j);
-      if (abs(val) < CHOP_TOL) val = 0.0;
+      if (abs(val) < chop_tol) val = 0.0;
       F << val << (j != dim2 - 1 ? " " : "");
     }
     F << std::endl;
@@ -147,74 +122,89 @@ void save_matrix(const std::string &filename, const MAT &M) {
   F.close();
 }
 
-void usage(std::ostream &F = std::cout)
-{
-  F << "Usage: unitary [-h] [-b | -B] [-qvV] [-tl] [-s scale] [-o output_fn] [-c chop_tol] <A> <B> <C>" << std::endl;
-}
+class Unitary {
+ private:
+   bool quiet            = false;
+   bool verbose          = false;
+   bool veryverbose      = false;
+   bool transpose_first  = false;
+   bool transpose_last   = false;
+   double scale_factor   = 1.0;
+   double chop_tol       = 1e-14;
+   bool input_ac_bin     = false;
+   std::optional<std::string> output_filename;
 
-void parse_param(int argc, char *argv[]) {
-  char c;
-  while (c = getopt(argc, argv, "hbBqvVtls:o:c:"), c != -1) {
-    switch (c) {
-      case 'h':
-        usage();
-        exit(EXIT_SUCCESS);
-      case 'b': input_ac_bin = false; break;
-      case 'B': input_ac_bin = true; break;
-      case 'q': quiet = true; break;
-      case 'v': verbose = true; break;
-      case 'V': veryverbose = true; break;
-      case 't': transpose_first = true; break;
-      case 'l': transpose_last = true; break;
-      case 's': scale_factor = atof(optarg); break;
-      case 'o': output_filename = optarg; break;
-      case 'c': CHOP_TOL = atof(optarg); break;
-      default: throw std::runtime_error("Unknown argument " + c);
-    }
-  }
-}
+   void usage(std::ostream &F = std::cout) {
+     F << "Usage: unitary [-h] [-b | -B] [-qvV] [-tl] [-s scale] [-o output_fn] [-c chop_tol] <A> <B> <C>" << std::endl;
+   }
+   
+   void parse_param(int argc, char *argv[]) {
+     char c;
+     while (c = getopt(argc, argv, "hbBqvVtls:o:c:"), c != -1) {
+       switch (c) {
+       case 'h':
+         usage();
+         exit(EXIT_SUCCESS);
+       case 'b': input_ac_bin = false; break;
+       case 'B': input_ac_bin = true; break;
+       case 'q': quiet = true; break;
+       case 'v': verbose = true; break;
+       case 'V': veryverbose = true; break;
+       case 't': transpose_first = true; break;
+       case 'l': transpose_last = true; break;
+       case 's': scale_factor = atof(optarg); break;
+       case 'o': output_filename = std::string(optarg); break;
+       case 'c': chop_tol = atof(optarg); break;
+       default: throw std::runtime_error("Unknown argument " + c);
+       }
+     }
+   }
+   
+   void about() {
+     if (!quiet) {
+       std::cout << "# unitary -- command line unitary transformation tool" << std::endl;
+       std::cout << "# Rok Zitko, rok.zitko@ijs.si, 2009-2020" << std::endl;
+     }
+   }
 
-void run(MAT &A, const MAT &B, MAT &C) // A and B may be transposed
-{
-  if (transpose_first) A = ublas::trans(A);
-  if (transpose_last) C = ublas::trans(C);
-  assert(A.size2() == B.size1());
-  assert(B.size2() == C.size1());
-  //    MAT N(B.size1(), C.size2());
-  MAT N = ublas::prod(B, C);
-  //    MAT M(A.size1(), C.size2());
-  MAT M = ublas::prod(A, N);
-  if (scale_factor != 1.0) M = scale_factor * M;
-  if (veryverbose) std::cout << "M=" << M << std::endl;
-  if (output_filename != nullptr) save_matrix(output_filename, M);
-}
+ public:
+   void run(MAT &A, const MAT &B, MAT &C) { // A and B may be transposed
+     if (transpose_first) A = ublas::trans(A);
+     if (transpose_last) C = ublas::trans(C);
+     assert(A.size2() == B.size1());
+     assert(B.size2() == C.size1());
+     MAT N = ublas::prod(B, C);
+     MAT M = ublas::prod(A, N);
+     if (scale_factor != 1.0) M = scale_factor * M;
+     if (veryverbose) std::cout << "M=" << M << std::endl;
+     if (output_filename) save_matrix(output_filename.value(), M, verbose, chop_tol);
+   }
 
-void run(const std::string &fnA, const std::string &fnB, const std::string &fnC) {
-  auto A = read_matrix(fnA, input_ac_bin);
-  const auto B = read_matrix(fnB);
-  auto C = read_matrix(fnC, input_ac_bin);
-  run(A, B, C);
-}
+   void run(const std::string &fnA, const std::string &fnB, const std::string &fnC) {
+     auto A = read_matrix(fnA, input_ac_bin, verbose, veryverbose);
+     const auto B = read_matrix(fnB, false);
+     auto C = read_matrix(fnC, input_ac_bin, verbose, veryverbose);
+     run(A, B, C);
+   }
 
-void run(int argc, char *argv[]) {
-  const auto remaining = argc - optind; // arguments left after switch parsing
-  if (remaining == 3) {
-    const std::string fnA = argv[optind];
-    const std::string fnB = argv[optind+1];
-    const std::string fnC = argv[optind+2];
-    run(fnA, fnB, fnC);
-  } else {
-    usage();
-  }
-}
-
-void about() {
-  if (!quiet) {
-    std::cout << "# unitary -- command line unitary transformation tool" << std::endl;
-    std::cout << "# Rok Zitko, rok.zitko@ijs.si, 2009-2020" << std::endl;
-  }
-}
-
+   void run(int argc, char *argv[]) {
+     const auto remaining = argc - optind; // arguments left after switch parsing
+     if (remaining == 3) {
+       const std::string fnA = argv[optind];
+       const std::string fnB = argv[optind+1];
+       const std::string fnC = argv[optind+2];
+       run(fnA, fnB, fnC);
+     } else {
+       usage();
+     }
+   }
+   
+   Unitary(int argc, char *argv[]) {
+     parse_param(argc, argv);
+     about();
+   }
+};
+   
 } // namespace
 
 #endif
