7383a7b98788b3769eea1f49dcb8c3b588cd2256
Rok Zitko <rok.zitko@ijs.si>
2020-12-10 15:40:10 +0100

    cdmI simplification

--- c++/dmnrg.hpp
+++ c++/dmnrg.hpp
@@ -62,20 +62,8 @@ void cdmI(const size_t i,        // Subspace index
           const Store<S> &store,
           const Params &P)
 {
-  my_assert(i < P.combs);
   nrglog('D', "cdmI i=" << i << " I1=" << I1 << " factor=" << factor);
-  // Range of indexes r and r' in matrix C^{QS,N}_{r,r'}, cf. Eq. (3.55) in my dissertation.
-  const auto dim = rhoNEW.size2();
-  // number of states taken into account in the density-matrix at *current* (Nth) stage (in subspace I1)
-  const auto nromega = rhoN.size2();
-  if (nromega == 0 || dim == 0) return;   // continue only if connection exists
-  // rmax (info[I1].rmax[i]) is the range of r in U^N_I1(omega|ri), only those states that we actually kept..
-  const auto rmax = store[N].at(I1).rmax.rmax(i);
-  if (rmax == 0) return;    // rmax can be zero in the case a subspace has been completely truncated
-  my_assert(rmax == dim);   // Otherwise, rmax must equal dim
-  // Check range of omega: do the dimensions of C^N_I1(omega omega') and U^N_I1(omega|r1) match?
-  my_assert(nromega <= diagI1.getnrstored());
-  const auto U = diagI1.vectors.submatrix({0, nromega}, store[N].at(I1).rmax.part(i)); // YYY: Ublock ??
+  const auto U = diagI1.U.get(i);
   rotate<S>(rhoNEW, factor, U, rhoN);
 }
 
