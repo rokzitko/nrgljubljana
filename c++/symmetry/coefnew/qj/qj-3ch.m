24d63861e0158fc1dfd75ec02b7210e30ddc3f34
Rok Zitko <rok.zitko@ijs.si>
2023-11-16 10:47:58 +0100

    revert to old versions of su2, spsu2c3, qst, qsc3

--- c++/symmetry/coefnew/qj/qj-3ch.m
+++
@@ -1,9 +0,0 @@
-(* Rok Zitko, rok.zitko@ijs.si, Mar 2016 *)
-
-<<sneg`;
-<<"../nrgcoef.m";
-
-channels=3;
-<<"qj.m";
-
-doall[];
--- c++/symmetry/coefnew/qsc3/qsc3-3ch.m
+++
@@ -1,9 +0,0 @@
-(* Rok Zitko, rok.zitko@ijs.si, Jan 2008 *)
-
-<<sneg`;
-<<"../nrgcoef.m";
-
-channels=3;
-<<"qsc3.m";
-
-doall[];
