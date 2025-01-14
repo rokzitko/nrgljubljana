d0a2920ffaf0f08f7324be58b2e68906c7dc3a0a
Rok Zitko <rok.zitko@ijs.si>
2025-01-14 13:15:05 +0100

    bug fix, missing = 0 for virtual function in DiagEngine; code cleanup; version bump

--- c++/diagengine.hpp
+++ c++/diagengine.hpp
@@ -17,7 +17,7 @@ template <scalar S>
 class DiagEngine {
  public:
    virtual DiagInfo<S> diagonalisations(const Step &step, const Opch<S> &opch, const Coef<S> &coef, const DiagInfo<S> &diagprev, const Output<S> &output,
-                                        const std::vector<Invar> &tasks, const DiagParams &DP, const Symmetry<S> *Sym, const Params &P);
+                                        const std::vector<Invar> &tasks, const DiagParams &DP, const Symmetry<S> *Sym, const Params &P) = 0;
    virtual ~DiagEngine() = default;
 };
 
