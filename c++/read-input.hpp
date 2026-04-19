89d0f93b96b588695467c3cb616585584045575e
Rok Zitko <rok.zitko@ijs.si>
2026-04-19 18:57:10 +0200

    decompose seed-state loading
    Break InputData construction into explicit header, symmetry, seed-data, block-reading, and coefficient-finalization phases so the import workflow is easier to follow and reason about. Keep the data-file semantics unchanged while removing constructor-only state and ad-hoc parsing helpers.

--- c++/read-input.hpp
+++ c++/read-input.hpp
@@ -21,33 +21,6 @@
 
 namespace NRG {
 
-// Parse the header of the data file, check the version, determine the symmetry type.
-inline auto parse_datafile_header(std::istream &fdata, const int expected_version = 9)
-{
-  std::string sym_string = "";
-  int dataversion = -1;
-  while (fdata.peek() == '#') {
-    fdata.ignore(); // ignore '#'
-    if (fdata.peek() == '!') {
-      fdata.ignore(); // ignore '!'
-      fdata >> dataversion;
-    } else {
-      std::string line;
-      std::getline(fdata, line);
-      auto pos = line.find("symtype", 1);
-      if (pos != std::string::npos) {
-        // Symmetry type declaration
-        auto p = line.find_last_of(" \t");
-        if (p != std::string::npos && p < line.size() - 1)
-          sym_string = line.substr(p + 1); // global variable
-      }
-    }
-    if (fdata.peek() == '\n') fdata.ignore();
-  }
-  my_assert(dataversion == expected_version);
-  return sym_string;
-}
-
 // Determine Nmax & Nlen, taking into account P.substeps. Must be called after the
 // tridiagonalization routines if not using the tables in the data file.
 template<scalar S>
@@ -60,21 +33,108 @@ void determine_Nmax_Nlen(const Coef<S> &coef, const size_t Nmax0, Params &P) { /
   my_assert(P.Nlen < MAX_NDX);
 }
 
-inline auto get_next_block(std::istream &fdata) {
-  while (!fdata.eof() && std::isspace(fdata.peek())) fdata.get();       // skip white space
-  char ch = char(fdata.get());
-  std::string opname;
-  std::getline(fdata, opname);
-  return std::make_pair(ch, opname);
-}
-
 template<scalar S>
 class InputData {
 private:
-  std::string sym_string;
-  size_t channels;
-  size_t Nmax;
-  size_t nsubs;
+  struct HeaderInfo {
+    std::string sym_string;
+    size_t channels;
+    size_t Nmax;
+    size_t nsubs;
+  };
+
+  static auto read_header(std::istream &fdata, const int expected_version = 9) {
+    std::string sym_string;
+    int dataversion = -1;
+    while (fdata.peek() == '#') {
+      fdata.ignore();
+      if (fdata.peek() == '!') {
+        fdata.ignore();
+        fdata >> dataversion;
+      } else {
+        std::string line;
+        std::getline(fdata, line);
+        if (const auto pos = line.find("symtype", 1); pos != std::string::npos) {
+          if (const auto p = line.find_last_of(" \t"); p != std::string::npos && p < line.size() - 1)
+            sym_string = line.substr(p + 1);
+        }
+      }
+      if (fdata.peek() == '\n') fdata.ignore();
+    }
+    my_assert(dataversion == expected_version);
+    return HeaderInfo{sym_string, read_one<size_t>(fdata), read_one<size_t>(fdata), read_one<size_t>(fdata)};
+  }
+
+  static auto read_next_block(std::istream &fdata) {
+    while (!fdata.eof() && std::isspace(fdata.peek())) fdata.get();
+    char ch = char(fdata.get());
+    std::string opname;
+    std::getline(fdata, opname);
+    return std::make_pair(ch, opname);
+  }
+
+  void initialize_symmetry(Params &P, const HeaderInfo &header) {
+    my_assert(header.sym_string == P.symtype.value());
+    Sym = set_symmetry<S>(P, header.sym_string, header.channels);
+  }
+
+  void read_seed_data(std::istream &fdata, const HeaderInfo &header, Params &P) {
+    diag = DiagInfo<S>(fdata, header.nsubs, P);
+    if (!P.silent)
+      diag.states_report(Sym->multfnc());
+    operators.opch = Opch<S>(fdata, diag, P);
+  }
+
+  void read_blocks(std::istream &fdata, Params &P) {
+    while (true) {
+      const auto [ch, opname] = read_next_block(fdata);
+      if (fdata.eof()) break;
+      if (ch != '#' && !P.silent) std::cout << "Reading <||" << opname << "||> (" << ch << ")" << std::endl;
+      switch (ch) {
+      case '#':
+        break;
+      case 'e': GS_energy = read_one<double>(fdata); break;
+      case 's': operators.ops[opname]  = MatrixElements<S>(fdata, diag); break;
+      case 'p': operators.opsp[opname] = MatrixElements<S>(fdata, diag); break;
+      case 'g': operators.opsg[opname] = MatrixElements<S>(fdata, diag); break;
+      case 'd': operators.opd[opname]  = MatrixElements<S>(fdata, diag); break;
+      case 't': operators.opt[opname]  = MatrixElements<S>(fdata, diag); break;
+      case 'o': operators.opot[opname] = MatrixElements<S>(fdata, diag); break;
+      case 'q': operators.opq[opname]  = MatrixElements<S>(fdata, diag); break;
+      case 'z':
+        coef.xi.read(fdata, P.coefchannels);
+        coef.zeta.read(fdata, P.coefchannels);
+        break;
+      case 'Z':
+        coef.delta.read(fdata, P.coefchannels);
+        coef.kappa.read(fdata, P.coefchannels);
+        break;
+      case 'X':
+        coef.xiR.read(fdata, P.coefchannels);
+        coef.zetaR.read(fdata, P.coefchannels);
+        break;
+      case 'T':
+        coef.ep.read(fdata, P.coefchannels);
+        coef.em.read(fdata, P.coefchannels);
+        coef.u0p.read(fdata, P.coefchannels);
+        coef.u0m.read(fdata, P.coefchannels);
+        break;
+#ifdef CHAIN_COEF
+      case 'W':
+        coef.eps.read(fdata);
+        coef.t.read(fdata);
+        break;
+#endif
+      default: throw std::invalid_argument(fmt::format("Unknown block {} in data file.", ch));
+      }
+    }
+  }
+
+  void finalize_coefficients(Params &P, const HeaderInfo &header) {
+    if (std::string(P.tri) == "cpp") Tridiag<S>(coef, header.Nmax, P);
+    determine_Nmax_Nlen(coef, header.Nmax, P);
+    P.validate_after_data_file();
+  }
 public:
   std::shared_ptr<Symmetry<S>> Sym;
   DiagInfo<S> diag;
@@ -84,61 +144,11 @@ public:
   InputData(Params &P, const std::string &filename = "data") : coef(P) {
     std::ifstream fdata(filename);
     if (!fdata) throw std::runtime_error("Can't load initial data.");
-    sym_string = parse_datafile_header(fdata);
-    channels   = read_one<size_t>(fdata); // Number of channels in the bath
-    Nmax       = read_one<size_t>(fdata); // Length of the Wilson chain
-    nsubs      = read_one<size_t>(fdata); // Number of invariant subspaces
-    my_assert(sym_string == P.symtype.value()); // Check consistency between 'param' and 'data' file
-    Sym = set_symmetry<S>(P, sym_string, channels);
-    diag = DiagInfo<S>(fdata, nsubs, P); // 0-th step of the NRG iteration
-    if (!P.silent) 
-      diag.states_report(Sym->multfnc());
-    operators.opch = Opch<S>(fdata, diag, P);
-    while (true) {
-      const auto [ch, opname] = get_next_block(fdata);
-      if (fdata.eof()) break;
-      if (ch != '#' && !P.silent) std::cout << "Reading <||" << opname << "||> (" << ch << ")" << std::endl;
-      switch (ch) {
-        case '#':
-          break; // ignore embedded comment lines
-        case 'e': GS_energy = read_one<double>(fdata); break;
-        case 's': operators.ops[opname]  = MatrixElements<S>(fdata, diag); break;
-        case 'p': operators.opsp[opname] = MatrixElements<S>(fdata, diag); break;
-        case 'g': operators.opsg[opname] = MatrixElements<S>(fdata, diag); break;
-        case 'd': operators.opd[opname]  = MatrixElements<S>(fdata, diag); break;
-        case 't': operators.opt[opname]  = MatrixElements<S>(fdata, diag); break;
-        case 'o': operators.opot[opname] = MatrixElements<S>(fdata, diag); break;
-        case 'q': operators.opq[opname]  = MatrixElements<S>(fdata, diag); break;
-        case 'z':
-          coef.xi.read(fdata, P.coefchannels);
-          coef.zeta.read(fdata, P.coefchannels);
-          break;
-        case 'Z':
-          coef.delta.read(fdata, P.coefchannels);
-          coef.kappa.read(fdata, P.coefchannels);
-          break;
-        case 'X':
-          coef.xiR.read(fdata, P.coefchannels);
-          coef.zetaR.read(fdata, P.coefchannels);
-          break;
-        case 'T':
-          coef.ep.read(fdata, P.coefchannels);
-          coef.em.read(fdata, P.coefchannels);
-          coef.u0p.read(fdata, P.coefchannels);
-          coef.u0m.read(fdata, P.coefchannels);
-          break;
-#ifdef CHAIN_COEF
-        case 'W':
-          coef.eps.read(fdata);
-          coef.t.read(fdata);
-          break;
-#endif
-      default: throw std::invalid_argument(fmt::format("Unknown block {} in data file.", ch));
-      }
-    }
-    if (std::string(P.tri) == "cpp") Tridiag<S>(coef, Nmax, P); // before calling determine_Nmax_Nlen()
-    determine_Nmax_Nlen(coef, Nmax, P);
-    P.validate_after_data_file();
+    const auto header = read_header(fdata);
+    initialize_symmetry(P, header);
+    read_seed_data(fdata, header, P);
+    read_blocks(fdata, P);
+    finalize_coefficients(P, header);
   };
 };
 
