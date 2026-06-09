03f38825e5793fc5e4889c85758140a1847d5de5
Rok Zitko <rok.zitko@ijs.si>
2026-06-09 12:55:32 +0200

    Fix BLAS symbol naming detection

--- c++/linalg.hpp
+++ c++/linalg.hpp
@@ -24,45 +24,51 @@ using lapack_complex_double = std::complex<double>;
 #define LAPACK_zheevd NRG_LAPACK_zheevd
 #define LAPACK_zheevr NRG_LAPACK_zheevr
 
+#if defined(NRG_LAPACK_SYMBOL_SUFFIX_NONE)
+#define NRG_LAPACK_SYMBOL(name) #name
+#else
+#define NRG_LAPACK_SYMBOL(name) #name "_"
+#endif
+
 extern "C" {
 
 void NRG_LAPACK_dgemm(const char *transa, const char *transb, const lapack_int *m, const lapack_int *n, const lapack_int *k,
                       const double *alpha, const double *a, const lapack_int *lda, const double *b, const lapack_int *ldb,
-                      const double *beta, double *c, const lapack_int *ldc) __asm__("dgemm_");
+                      const double *beta, double *c, const lapack_int *ldc) __asm__(NRG_LAPACK_SYMBOL(dgemm));
 
 void NRG_LAPACK_zgemm(const char *transa, const char *transb, const lapack_int *m, const lapack_int *n, const lapack_int *k,
                       const lapack_complex_double *alpha, const lapack_complex_double *a, const lapack_int *lda,
                       const lapack_complex_double *b, const lapack_int *ldb, const lapack_complex_double *beta,
-                      lapack_complex_double *c, const lapack_int *ldc) __asm__("zgemm_");
+                      lapack_complex_double *c, const lapack_int *ldc) __asm__(NRG_LAPACK_SYMBOL(zgemm));
 
 void NRG_LAPACK_dsyev(const char *jobz, const char *uplo, const lapack_int *n, double *a, const lapack_int *lda, double *w,
-                      double *work, const lapack_int *lwork, lapack_int *info) __asm__("dsyev_");
+                      double *work, const lapack_int *lwork, lapack_int *info) __asm__(NRG_LAPACK_SYMBOL(dsyev));
 
 void NRG_LAPACK_dsyevd(const char *jobz, const char *uplo, const lapack_int *n, double *a, const lapack_int *lda, double *w,
                        double *work, const lapack_int *lwork, lapack_int *iwork, const lapack_int *liwork, lapack_int *info)
-  __asm__("dsyevd_");
+  __asm__(NRG_LAPACK_SYMBOL(dsyevd));
 
 void NRG_LAPACK_dsyevr(const char *jobz, const char *range, const char *uplo, const lapack_int *n, double *a,
                        const lapack_int *lda, const double *vl, const double *vu, const lapack_int *il,
                        const lapack_int *iu, const double *abstol, lapack_int *m, double *w, double *z,
                        const lapack_int *ldz, lapack_int *isuppz, double *work, const lapack_int *lwork,
-                       lapack_int *iwork, const lapack_int *liwork, lapack_int *info) __asm__("dsyevr_");
+                       lapack_int *iwork, const lapack_int *liwork, lapack_int *info) __asm__(NRG_LAPACK_SYMBOL(dsyevr));
 
 void NRG_LAPACK_zheev(const char *jobz, const char *uplo, const lapack_int *n, lapack_complex_double *a,
                       const lapack_int *lda, double *w, lapack_complex_double *work, const lapack_int *lwork,
-                      double *rwork, lapack_int *info) __asm__("zheev_");
+                      double *rwork, lapack_int *info) __asm__(NRG_LAPACK_SYMBOL(zheev));
 
 void NRG_LAPACK_zheevd(const char *jobz, const char *uplo, const lapack_int *n, lapack_complex_double *a,
                        const lapack_int *lda, double *w, lapack_complex_double *work, const lapack_int *lwork,
                        double *rwork, const lapack_int *lrwork, lapack_int *iwork, const lapack_int *liwork,
-                       lapack_int *info) __asm__("zheevd_");
+                       lapack_int *info) __asm__(NRG_LAPACK_SYMBOL(zheevd));
 
 void NRG_LAPACK_zheevr(const char *jobz, const char *range, const char *uplo, const lapack_int *n, lapack_complex_double *a,
                        const lapack_int *lda, const double *vl, const double *vu, const lapack_int *il,
                        const lapack_int *iu, const double *abstol, lapack_int *m, double *w, lapack_complex_double *z,
                        const lapack_int *ldz, lapack_int *isuppz, lapack_complex_double *work, const lapack_int *lwork,
                        double *rwork, const lapack_int *lrwork, lapack_int *iwork, const lapack_int *liwork,
-                       lapack_int *info) __asm__("zheevr_");
+                       lapack_int *info) __asm__(NRG_LAPACK_SYMBOL(zheevr));
 
 }
 
