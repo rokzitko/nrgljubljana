5f8b0af94ae9a0e8c0ffddacb9cf638b3f444376
Rok Zitko <rok.zitko@ijs.si>
2026-07-01 17:16:42 +0200

    add FDM to param check

--- c++/params.hpp
+++ c++/params.hpp
@@ -627,7 +627,7 @@ class Params {
     my_assert(Lambda > 1.0);
     if (diag == "dsyevr"s || diag =="zheevr"s) {
       my_assert(0.0 < diagratio && diagratio <= 1.0);
-      if (cfs_flags() && diagratio != 1.0) throw std::invalid_argument("CFS/FDM is not compatible with partial diagonalisation.");
+      if (cfs_or_fdm_flags() && diagratio != 1.0) throw std::invalid_argument("CFS/FDM is not compatible with partial diagonalisation.");
     }
     mult = to_lower(std::string(mult));
     if (mult != "blas"s && mult != "cuda"s) throw std::invalid_argument(fmt::format("Unsupported mult backend: {}", mult.value()));
